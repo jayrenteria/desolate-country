be4a0318d637b7ab533b3955d2384760940c8d55
Jay Renteria <jay.renteria@call2test.com>
2020-12-30 11:30:45 -0700

    ADDS: initial map component

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+import GoogleMapReact from 'google-map-react';
+
+function Map() {
+
+    const defaults = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    }
+
+
+    return(
+        <div style={{width: '100%', height: '100%'}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={defaults.center}
+                defaultZoom={defaults.zoom}
+            />
+        </div>
+    )
+}
+
+export default Map;
