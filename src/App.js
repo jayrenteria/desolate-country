1f0cf59d7834f9edfa2157a67910c06bd7df5f19
Eric Debelak <eric.debelak@call2test.com>
2020-12-30 11:21:30 -0700

    Adds a basic filter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Filter from './Filter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Filter label={"Numbers"} setSelected={result => console.log(result)} items={["One", "Two"]}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
