fb9808ffa3f5bd884be573ee1492b1c882265c5b
Eric Debelak <eric.debelak@call2test.com>
2021-01-27 12:46:22 -0700

    Add a way to group markers

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -13,6 +13,28 @@ function Map({dataToShow}) {
         zoom: 5
     }
 
+    let data = {}
+    for (let i = 0; i < dataToShow.length; i++) {
+        const item = dataToShow[i];
+        console.log(item)
+        if ( !data[`${item.latitude}-${item.longitude}-${item.name}`]) {
+            data[`${item.latitude}-${item.longitude}-${item.name}`] = {
+                name_of_institution: item.name_of_institution,
+                name: item.name,
+                latitude: item.latitude,
+                longitude: item.longitude,
+                institution_type: item.institution_type,
+                native_serving_mission: item.native_serving_mission,
+                abuse_claim: item.native_serving_mission,
+                years: [item]
+            }
+        }
+        data[`${item.latitude}-${item.longitude}-${item.name}`].years.push(item)
+        if (!data[`${item.latitude}-${item.longitude}-${item.name}`].abuse_claim && item.abuse_claim) {
+            data[`${item.latitude}-${item.longitude}-${item.name}`].abuse_claim = true;
+        }
+    }
+
     return(
         <div style={{width: '100%', height: '800px'}}>
             <GoogleMapReact
@@ -20,14 +42,14 @@ function Map({dataToShow}) {
                 defaultCenter={defaults.center}
                 defaultZoom={defaults.zoom}
             >
-                {dataToShow.map((institution, index) => {
+                {Object.values(data).map(item => {
                     return(
                         <Marker
-                            key={`${institution}-${index}`}
-                            lat={institution.latitude}
-                            lng={institution.longitude}
-                            text={institution.name_of_institution}
-                            institution={institution}
+                            key={`${item.name_of_institution}-${item.name}`}
+                            lat={item.latitude}
+                            lng={item.longitude}
+                            text={item.name_of_institution}
+                            institution={item}
                         />
                     )
                 })}
