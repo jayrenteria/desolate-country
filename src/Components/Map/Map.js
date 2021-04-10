c209b1b37fbfc20d07e6b8b9a7e22a1907d45a0e
Jay Renteria <jay.renteria@call2test.com>
2021-04-10 13:06:24 -0600

    ADDS: toggle heatmap button

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,9 +1,15 @@
-import React from 'react';
+/* global google */
+import React, {useState, useRef} from 'react';
 import GoogleMapReact from 'google-map-react';
+import Button from '@material-ui/core/Button';
 
 import Marker from '../Marker/Marker';
 
+import './styles.css';
+
 function Map({dataToShow, setInstitution}) {
+    const [heatMapVisible, setHeatMapVisible] = useState();
+    const mapEl = useRef(null);
     // US default center/zoom
     const defaults = {
         center: {
@@ -13,7 +19,24 @@ function Map({dataToShow, setInstitution}) {
         zoom: 5
     }
 
+    const toggleHeatMap = () => {
+        
+        setHeatMapVisible(!heatMapVisible);
+        if (mapEl !== undefined) {
+            console.log(mapEl.current.heatmap)   
+            mapEl.current.heatmap.setMap(heatMapVisible ?
+                mapEl.current.map_ : null)      
+        }
+    }
+
     let data = {}
+    let heatMapData = {   
+        positions: [],
+        options: {   
+            radius: 40,   
+            opacity: 0.6,
+        }
+    }
     for (let i = 0; i < dataToShow.length; i++) {
         const item = dataToShow[i];
         if ( !data[`${item.latitude}-${item.longitude}`]) {
@@ -31,14 +54,19 @@ function Map({dataToShow, setInstitution}) {
         if (!data[`${item.latitude}-${item.longitude}`].abuse_claim && item.abuse_claim) {
             data[`${item.latitude}-${item.longitude}`].abuse_claim = true;
         }
+        heatMapData.positions.push({ lat:item.latitude, lng: item.longitude });
     }
 
     return(
-        <div style={{width: '100%', height: '800px'}}>
+        <div style={{width: '100%', height: '800px', position: 'relative'}}>
+            <Button className="toggle-heatmap" variant="contained" color="primary" onClick={() => toggleHeatMap()}>Heatmap</Button>
             <GoogleMapReact
+                ref={mapEl}
                 bootstrapURLKeys={{ key: '' }}
                 defaultCenter={defaults.center}
                 defaultZoom={defaults.zoom}
+                heatmapLibrary={true}          
+                heatmap={heatMapData}
             >
                 {Object.values(data).map(item => {
                     return(
