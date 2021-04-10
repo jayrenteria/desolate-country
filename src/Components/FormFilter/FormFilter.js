47074e2e693087e971d21293bc6a867935e0de30
Eric Debelak <eric.debelak@call2test.com>
2021-04-10 11:57:43 -0600

    fix bad key

--- src/Components/FormFilter/FormFilter.js
+++ src/Components/FormFilter/FormFilter.js
@@ -14,12 +14,14 @@ function FormFilter({
 }) {
 
     return (
-        <FormGroup>
-            <Typography>Filters</Typography>
-            <Filter label={'Name'} setSelected={result => updateSort('name', result)} items={names} selected={sortValues['name']}/>
-            <Filter label={'Institution'} setSelected={result => updateSort('name_of_institution', result)} items={names_of_institutions} selected={sortValues['name_of_institution']}/>
-            <RangeFilter label={'Year Range'} setSelected={result => updateSort('year', result)} items={years}/>
-        </FormGroup>
+        <div>
+            <FormGroup>
+                <Typography>Filters</Typography>
+                <Filter label={'Name'} setSelected={result => updateSort('name', result)} items={names} selected={sortValues['name']}/>
+                <Filter label={'Institution'} setSelected={result => updateSort('name_of_institution', result)} items={names_of_institutions} selected={sortValues['name_of_institution']}/>
+                <RangeFilter label={'Year Range'} setSelected={result => updateSort('year', result)} items={years}/>
+            </FormGroup>
+        </div>
     )
 };
 
