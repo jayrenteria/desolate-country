b0613108fe9b663e5257b8a61300fd5274df2363
Eric Debelak <eric.debelak@call2test.com>
2022-02-14 13:19:22 -0700

    Adds box shadow on markers

--- src/Components/Marker/Marker.js
+++ src/Components/Marker/Marker.js
@@ -7,9 +7,9 @@ function Marker ({ institution, setInstitution }) {
   return (
     <div className='marker' onClick={() => setInstitution(institution)} style={{zIndex: institution.abuse_claims}}>
       {institution.abuse_claims ? 
-            <div className='claim'>{institution.abuse_claims}</div>
+            <div className='dot claim'>{institution.abuse_claims}</div>
           :
-            <div className='no-claim'/>
+            <div className='dot no-claim'/>
       }
     </div> 
   )
